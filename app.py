f832209a5804058d91d76666dca206f0dd38d2b5
Daniel Bascón Arenas <bascon1991@hotmail.es>
2017-04-17 14:40:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ bottle.debug(True)
 
 @route('/')
 def index():
-	return "<h1>Hola a todos!!!</h1>"
+	return "<h1>Hola a tod@s!!!</h1>"
 
 if __name__ == '__main__':
 	run(host='0.0.0.0',port=argv[1])
